70bac2dfcb14562f22cb9e52cfcc2dcdc438bfe3
sergey080888 <237264@mail.ru>
2022-06-24 11:23:34 +0300

    first

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import xml.etree.ElementTree as ET
 import collections
-
-def read_xml("newsafr.xml", max_len_word=6, top_words=10)
+def reader():
     parser = ET.XMLParser(encoding='utf-8')
     tree = ET.parse("newsafr.xml", parser)
     root = tree.getroot()
     descriptors_list = root.findall("channel/item/description")
+    q =[]
+    for i in descriptors_list:
+        q.append(i.text)
+    print(q)
+reader()
+
+
+
